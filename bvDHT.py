10162290832b96d5fc1967ef825f904e570e005f
Elaine George <83991466+gelaico@users.noreply.github.com>
2023-04-21 16:50:00 -0500

    Progress on Closest
    Closest User input-algorithm and ClosestReceive Function it would appear that these functions seems to be sorted out though there are likely small bugs around variable and function names I need to correct, but I require help standardizing them to our code

--- bvDHT.py
+++ bvDHT.py
@@ -3,7 +3,6 @@ from sys import argv
 import threading
 import hashlib
 
-# hashedKey = hashlib.sha224(key.encode()).hexdigest()
 # int(hasedKey, base=16) <- gets the int version of the digest
 
 def getLine(conn):
@@ -15,7 +14,55 @@ def getLine(conn):
             break
     return msg.decode()
 
-def closest_peer(hashedpos, connInfo):
+def updatePeerList():
+    pass
+
+#inputs the non-encrypted Key.
+#returns closest peer's userID without a new line... str(ip) + ":" + str(port)
+def closestAlgorithim(key):
+    hashedKey = hashlib.sha224(key.encode()).hexdigest()
+    listy = knownPeers.items()
+    end = listy[0]
+    for peer in listy:
+        if peer[0] < hashedKey and peer[0] > end[0]:
+            end = peer
+    if end == listy[0]:
+        if listy[0][0] > hashedKey:
+            end = listy[-1]
+    connTuple = (end[1], int(end[2]))
+    val = str(end[1]) + ":" + str(end[2])
+    while True:
+        clientSock = socket(AF_INET, SOCK_STREAM)
+        clientSock.connect( conn_Tuple )
+        val = closestPeer(clientSocket)
+        splitVal = val.split(":")
+        if (splitVal[0],splitVal[1]) == connTuple:
+            break
+        connTuple = (splitVal[0],splitVal[1])
+    return val
+
+#so this works basically by looking through all of our knownPeers list, which should include us.
+# for each peer in our known peers
+#  check if that peer's ID is less than the hashedKey and greater than the last closest id.
+#  if thats the case, change the last closest id to the current
+#  otherwise continue
+# now if at the end the closest peer is the first, we check if it's actually larger than the ID. If it is, than we know the previous person is actually the holder of that ID
+# thus, we loop back around to the last peer in our list and return that peer's USERID
+
+###TODO:Note if knownPeers list does not contain ourselves than we need to include us in the search below explictly. ####
+def closestRecv(sockRecv):
+    hashedKey = getline(sock)
+    listy = knownPeers.items()
+    end = listy[0]
+    for peer in listy:
+        if peer[0] < hashedKey and peer[0] > end[0]:
+            end = peer
+    if end == listy[0]:
+        if listy[0][0] > hashedKey:
+            end = listy[-1]
+    sockRecv.send (str(end[1]) + ":" + (str(end[2]))  + "\n" )
+
+def closestPeer(hashedpos, connInfo):
     sock, sockAddress = connInfo
     sock.send('CLOSEST_PEER'.encode())
     sock.send(hashedpos)
@@ -139,7 +186,7 @@ ourID = f'{ourIP}:{ourPort}'
 data = {}
 nextID = ''
 
-#<clientIP>:<clientPort> 
+#clientID = clientIP:clientPort>
 clientID = input('Client ID: ')
 known_peers = [clientID]
 clientIP, clientPort = clientID.split(':')
